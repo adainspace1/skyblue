397bf772dda3d5c2e4c3928ecd0e90ce53d3b6d6
Shazaniyu55 <shazaniyu@gmail.com>
2024-01-30 09:31:26 +0100

    build

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -15,7 +15,7 @@ const ProductCard = () => {
           className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-8">
             <div className=" rounded-lg p-6 hover:scale-40 transform transition-transform duration-300">
               <div className="mb-4 relative">
-              <img src='/assets/img23.JPG'
+              <img src='/assets/img14.JPG'
               className="rounded-lg w-full h-auto"
               width={200} height={100}/>
                 
@@ -31,8 +31,7 @@ const ProductCard = () => {
               <motion.p
                
               className="text-gray-600 mb-4">
-                Sky Blue Table Water is a subcidiary or Apa property integrated
-                company that commence operation in july 2023 in design to and equipped to deliver premium quality and safe table Water
+    Our premium table water is sourced from the purest natural springs, ensuring a crisp and refreshing taste with every sip. Purified through state-of-the-art filtration processes, our water meets the highest quality standards. Packed in eco-friendly bottles, our table water is not just a thirst-quencher; it's a commitment to health, sustainability, and uncompromised purity.
 
               </motion.p>
             </div>
@@ -53,8 +52,9 @@ const ProductCard = () => {
                   }}
                 ></div>
               </div>
-              <h3 className="text-xl font-semibold mb-4"></h3>
+              <h3 className="text-xl font-semibold mb-4">Sky Bread</h3>
               <p className="text-gray-600 mb-4">
+              Immerse your senses in the aroma and taste of our freshly baked bread. Crafted with care and using only the finest ingredients, our bread is a testament to quality and flavor. From classic white and whole grain options to artisanal varieties, each loaf is baked to perfection, delivering a delightful crunch on the outside and a soft, fluffy texture on the inside. Experience the essence of tradition and innovation with every bite.
               </p>
             </div>
           </div>
@@ -74,34 +74,15 @@ const ProductCard = () => {
                   }}
                 ></div>
               </div>
-              <h3 className="text-xl font-semibold mb-4"></h3>
+              <h3 className="text-xl font-semibold mb-4">Sky Printing</h3>
               <p className="text-gray-600 mb-4">
-               
+              Elevate your brand with our comprehensive printing services. Whether you need eye-catching business cards, vibrant promotional materials, or professional stationery, we've got you covered. Our cutting-edge printing technology ensures precision and clarity in every print, making your visuals stand out. Collaborate with our design experts to bring your ideas to life, or choose from our range of templates. From concept to delivery, we're committed to transforming your vision into tangible, high-quality printed products.
+
+
               </p>
             </div>
           </div>
-          {/**card 4 */}
-           {/* Card 3 */}
-           <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-8">
-            <div className=" rounded-lg p-6 hover:scale-105 transform transition-transform duration-300">
-              <div className="mb-4 relative">
-              <img src='/assets/img14.JPG'
-              className="rounded-lg w-full h-auto"
-              width={200} height={100}/>
-                {/* Gradient Effect */}
-                <div
-                  className="absolute inset-0"
-                  style={{
-                    background: 'linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.5) 100%)',
-                  }}
-                > </div>
-              </div>
-              <h3 className="text-xl font-semibold mb-4"></h3>
-              <p className="text-gray-600 mb-4">
-                
-              </p>
-            </div>
-          </div>
+          
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,24 +3,17 @@ module.exports = {
   content: [ "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-
+      
     },
     colors: {
       'adainblack': '#000000',
       'adainyellow': '#76D2FF',
       'adainfooterblack': '#1B1919',
-      'space1-2': '#7c0a02',
-      'space1-3': '#371f76',
       'adainwhite': '#ffffff',
-      'space1-5': '#ebecf0',
-      'space1-6': '#ffc0cb',
-      'space1-7': '#4b0082',
-      'space1-8': '#7A687F',
-      'space1-9': '#581845',
-      'space1-10':'#ff7c00',
-      'space1-11':'#ff2600',
+      
 
     },
+    
   },
   plugins: [],
 }
