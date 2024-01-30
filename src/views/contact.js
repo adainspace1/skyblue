900bfb1a7d12f95c9c473edd34b828ba583d6cd9
Shazaniyu55 <shazaniyu@gmail.com>
2024-01-30 09:50:22 +0100

    animation

--- src/views/contact.js
+++ src/views/contact.js
@@ -2,6 +2,7 @@ import React from "react";
 import AdainNavBar from "../components/navbar";
 import Footer from "../components/footer";
 import { useState } from "react";
+import {motion} from 'framer-motion'
 
 
 
@@ -14,7 +15,13 @@ const Contact = ()=>{
     <section class="bg-white dark:bg-gray-900 m-10">
       <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
       <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
-      <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
+      <motion.p
+      initial={{x: -1000}}
+      animate={{x: 0}}
+      transition={{
+        duration: "2",
+      }}
+      class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</motion.p>
       <form action="#" class="space-y-8">
           <div>
               <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
